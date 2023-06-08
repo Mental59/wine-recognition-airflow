8c91bc6ee944e47c3d3d5acbe29413dd1ea0cf3b
Aleksey <Alekov99@mail.ru>
2023-06-08 18:06:16 +0500

    Add freq dict generating DAG

---
+++ dags/data_generation_dags/generate_freq_dict_from_transformed_data.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'mental',
+    'retries': 0,
+    'retry_delay': timedelta(seconds=10)
+}
+
+def generate_freq_dict(params, **kwargs):
+    import os
+    from lib.data_master import DataGenerator, DataLoader, DataSaver
+    from lib.utils import load_data_info, get_data_dir
+
+    os.chdir(get_data_dir())
+    conf: dict = kwargs['dag_run'].conf
+
+    data_info = load_data_info()
+
+    byword = params['byword']
+    freq_dict = DataGenerator.generate_freq_dict(
+        *[DataLoader.load_csv_data(path) for path in conf['data_paths']],
+        keys=data_info['keys']['all'],
+        byword=byword
+    )
+
+    output_folder = conf['output'] + '_byword' if byword else conf['output']
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+    
+    DataSaver.save_frequency_dictionary(
+        frequency_dictionary=freq_dict,
+        csv_folder=output_folder,
+        excel_path=output_folder + '.xlsx'
+    )
+        
+with DAG(
+    dag_id='generate_freq_dict_from_transformed_data',
+    default_args=default_args,
+    description='DAG for generating frequency dictionary from transformed source data',
+    start_date=datetime.now(),
+    schedule_interval=None,
+    catchup=False
+) as dag:
+    generate_byword_freq_dict_task = PythonOperator(
+        task_id='generate_byword_freq_dict',
+        provide_context=True,
+        params={
+            'byword': True
+        },
+        python_callable=generate_freq_dict
+    )
+    
+    generate_freq_dict_task = PythonOperator(
+        task_id='generate_freq_dict',
+        provide_context=True,
+        params={
+            'byword': False
+        },
+        python_callable=generate_freq_dict
+    )
--- dags/lib/utils/__init__.py
+++ dags/lib/utils/__init__.py
@@ -1,8 +1,14 @@
-def get_data_dir():
-    import os
+import os
+import json
 
+def get_data_dir():
     data_dir = os.getenv('AIRFLOW_DATA_PATH')
+
     if data_dir is None:
         raise Exception('AIRFLOW_DATA_PATH is None')
     
     return data_dir
+
+def load_data_info():
+    data_dir = get_data_dir()
+    return json.load(open(os.path.join(data_dir, 'data_info', 'data_info.json')))
